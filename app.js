170d5ab763e7213ca8bc82ee69cd70252bc20509
salima <brycesalima@gmail.com>
2024-01-11 14:34:38 +0000

    working on conditionals

---
+++ app.js
@@ -0,0 +1,12 @@
+let age = 17;
+let movieRating = "PG";
+
+if (age > 17) {
+  console.log("You can watch any movie!");
+} else if (age === 17) {
+  if (movieRating === "PG") {
+    console.log("You can watch the movie!");
+    if (movieRating != "U" || "12" || "15" || "18") console;
+    console.log("Enjoy the movie! ");
+  }
+}
